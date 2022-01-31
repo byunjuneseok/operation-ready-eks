1c4de745fb8057d4912c40c67860c6df97ff6b97
byunjuneseok <byunjuneseok@gmail.com>
2022-02-01 00:41:13 +0900

    Add helm provider

--- eks/provider.tf
+++ eks/provider.tf
@@ -1,16 +1,20 @@
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    aws        = {
+      source  = "hashicorp/aws"
       version = "3.74.0"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.7.1"
     }
-    null = {
-      source = "hashicorp/null"
+    null       = {
+      source  = "hashicorp/null"
       version = "3.1.0"
+    }
+    helm       = {
+      source  = "hashicorp/helm"
+      version = "2.4.1"
     }
   }
 }
@@ -27,4 +31,12 @@ provider "kubernetes" {
 }
 
 provider "null" {
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
 }
